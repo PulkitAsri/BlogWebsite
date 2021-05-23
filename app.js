78309a17cddd7595b476b5c02b533d88a0617cc3
PulkitAsri <pulkit.asri@gmail.com>
2021-05-24 00:10:03 +0530

    Working condition

--- app.js
+++ app.js
@@ -3,7 +3,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-var _ = require('lodash');
+const _ = require('lodash');
+const mongoose=require('mongoose');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -19,15 +20,41 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 
 
+
+// CODE.............................................
+
+const connectionUrl="mongodb://localhost:27017/blogDatabase";
+
+mongoose.connect(connectionUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+//BLOG SCHEMA
+const blogSchema={
+  title:String,
+  body:String
+};
+const Blog=mongoose.model("Blog",blogSchema);
+
+
+
+
 //Array
 let posts = [];
 
 app.get("/", function (req, res) {
-  res.render("home", {
-    homeContent: homeStartingContent,
-    postArray: posts
+  Blog.find({},function(err,blogArray){
+
+    res.render("home", {
+      homeContent: homeStartingContent,
+      postArray: blogArray
+    });
 
   });
+  
+
+  
 });
 
 app.get("/contact", function (req, res) {
@@ -43,44 +70,43 @@ app.get("/about", function (req, res) {
 
   });
 });
+
+
+//COMPOSE
 app.get("/compose", function (req, res) {
   res.render("compose");
 });
-app.get("/posts/:title", function (req, res) {
-
-  let requestedTitle=_.kebabCase(req.params.title);
-  posts.forEach(e => {
-    let originalTitle = _.kebabCase(e.title);
-    if (originalTitle === requestedTitle) {
-      res.render("post", {
-        blogTitle: e.title,
-        blogBody: e.body
-      });
-    } else {
-
-
-    }
-  });
-});
-
-
-
-
-
-
-
 
 app.post("/compose", function (req, res) {
-  let post = {
+  const post =new Blog( {
     title: req.body.titleNewBlog,
     body: req.body.bodyNewBlog
-  };
-  posts.push(post);
-  res.redirect("/");
+  });
+  post.save(function(err){
+    if(!err){
+      res.redirect("/");
+    }
+  });
+
+});
+
+//CUSTOM ROUTES
+app.get("/posts/:id_of_blog", function (req, res) {
+
+  let parameter=req.params.id_of_blog;
+
+  Blog.findById(parameter,function(err,blog){
+    res.render("post", {
+      blogTitle: blog.title,
+      blogBody: blog.body
+    });
+
+  });
 
 });
 
 
+//SERVER
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
